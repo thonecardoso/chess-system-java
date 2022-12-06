a40583f41265f9d2c3d8348bafc513b8294809c3
Thone <46010284+thonecardoso@users.noreply.github.com>
2022-12-06 14:40:45 -0300

    First class: Position

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        var position = new Position(3,5);
+        System.out.println(position);
+    }
+}
