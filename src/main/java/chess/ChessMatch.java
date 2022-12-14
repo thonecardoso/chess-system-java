b27bbd49cc2525098f87a7649d8fb3e7c3feceac
Thone <46010284+thonecardoso@users.noreply.github.com>
2022-12-14 15:31:50 -0300

    Implement turn

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -18,7 +18,17 @@ public class ChessMatch {
 
     public ChessMatch() {
         this.board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
 
     public ChessPiece[][] getPieces() {
@@ -43,7 +53,13 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         var capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece) capturedPiece;
+    }
+
+    private void nextTurn() {
+        turn ++;
+        currentPlayer = currentPlayer == Color.WHITE ? Color.BLACK : Color.WHITE;
     }
 
     private Piece makeMove(Position source, Position target) {
@@ -56,6 +72,9 @@ public class ChessMatch {
     private void validateSourcePosition(Position position) {
         if(!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source position.");
+        }
+        if(((ChessPiece)board.piece(position)).getColor() != currentPlayer){
+            throw new ChessException("The chosen piece is not yours");
         }
         if(!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("There is no possible moves for the chosen piece.");
